9318ace01dc36ae77036a761a47d207b20c6b0ad
kellydunn <defaultstring@gmail.com>
2012-07-28 13:39:43 -0700

    [init]

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	clang -I/usr/lib/clang/2.8/include -I/usr/include/ src/warlock.cpp -o bin/warlock
---
+++ src/warlock.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <aubio/aubio.h>
+#include <freej.h>
+
+int main() {
+  printf("ho-ah\n");
+  return 0;
+}
