447007f368d305a4302b7a0c5ba76782b18e5fe3
kellydunn <defaultstring@gmail.com>
2012-07-28 18:11:50 -0700

    [src] Adding in some basic file consumption

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	clang -I/usr/lib/clang/2.8/include -I/usr/include/ src/warlock.cpp -o bin/warlock
+	clang -I/usr/lib/clang/2.8/include -I/usr/include/ src/warlock.cpp -o bin/warlock -lsndfile
--- src/warlock.cpp
+++ src/warlock.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <sndfile.h>
 #include <aubio/aubio.h>
+#include <aubio/sample.h>
+#include <aubio/sndfileio.h>
 #include <freej.h>
 
 typedef struct _aubio_sndfile_t aubio_sndfile_t;
@@ -13,7 +15,13 @@ struct _aubio_sndfile_t {
   int size;
 };
 
-int main() {
-  aubio_sndfile_t * f = AUBIO_NEW(aubio_sndfile_t);
+int main(int argc, char **argv) {
+  SF_INFO sf_info;
+  SNDFILE * file = sf_open("hustle_main_sample.wav", SFM_READ, &sf_info);
+  sf_count_t frames;
+  float *tmpdata;
+  smpl_t *pread;
+  sf_read_float(file, tmpdata, 1024);
+  printf("%d\n", sizeof(tmpdata));
   return 0;
 }
