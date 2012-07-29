88f3c62f680b04fcbd5826a5744b2b200608cfe5
kellydunn <defaultstring@gmail.com>
2012-07-28 17:28:45 -0700

    [config] setting ground for autoconf

--- src/warlock.cpp
+++ src/warlock.cpp
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <sndfile.h>
 #include <aubio/aubio.h>
 #include <freej.h>
 
+typedef struct _aubio_sndfile_t aubio_sndfile_t;
+struct _aubio_sndfile_t {
+  SNDFILE *handle;
+  int samplerate;
+  int channels;
+  int format;
+  float *tmpdata;
+  int size;
+};
+
 int main() {
-  printf("ho-ah\n");
+  aubio_sndfile_t * f = AUBIO_NEW(aubio_sndfile_t);
   return 0;
 }
