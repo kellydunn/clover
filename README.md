e8d6eb2cf94409a70126d3b78841c7d0e2016a7e
kellydunn <defaultstring@gmail.com>
2012-07-28 18:35:43 -0700

    [README][src] going in a different direciton

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 ## what
 
 Don't worry your pretty little head about it.
+
+## deps
+
+- ljack
+- laubio
+- lfreej
+
+## roadmap
+
+- jack => aubio => freej
--- src/warlock.cpp
+++ src/warlock.cpp
@@ -1,27 +1,16 @@
 #include <stdio.h>
 #include <sndfile.h>
 #include <aubio/aubio.h>
-#include <aubio/sample.h>
-#include <aubio/sndfileio.h>
 #include <freej.h>
-
-typedef struct _aubio_sndfile_t aubio_sndfile_t;
-struct _aubio_sndfile_t {
-  SNDFILE *handle;
-  int samplerate;
-  int channels;
-  int format;
-  float *tmpdata;
-  int size;
-};
 
 int main(int argc, char **argv) {
   SF_INFO sf_info;
-  SNDFILE * file = sf_open("hustle_main_sample.wav", SFM_READ, &sf_info);
+  SNDFILE *file = sf_open("hustle_main_sample.wav", SFM_READ, &sf_info);
   sf_count_t frames;
   float *tmpdata;
   smpl_t *pread;
   sf_read_float(file, tmpdata, 1024);
-  printf("%d\n", sizeof(tmpdata));
+
+  printf("%d\n", sizeof(pread));
   return 0;
 }
