61d706bf8c9f08fc4d99d6058c30b15fd5034c18
kellydunn <defaultstring@gmail.com>
2012-07-28 21:34:06 -0700

    [src] Adding in some fft shit

--- src/warlock.cpp
+++ src/warlock.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <jack/jack.h>
 #include <sndfile.h>
 #include <freej.h>
@@ -8,14 +9,29 @@ jack_options_t options = JackNoStartServer;
 jack_status_t status;
 jack_port_t *input_port;
 jack_port_t *output_port;
-
+jack_nframes_t *frames;
 const char *client_name = "warlock";
 const char *server_name = NULL;
+const char **ports;
 
-int process(jack_nframes_t frames, void *args){
+const int PI = 3.14159254;
+
+// Shamlessly ripped from the interwebs
+//   von Hann window
+double window(jack_default_audio_sample_t in, int n) {
+  return .5 * (1 - cos(2*PI*n/(int)frames)) * (double)in;
+}
+
+int process(jack_nframes_t nframes, void *args){
   jack_default_audio_sample_t *output;
-  output = (jack_default_audio_sample_t *)jack_port_get_buffer(output_port, frames);
-  printf("%d\n", sizeof(output));
+  double *fftw_in;
+  int i;
+
+  output = (jack_default_audio_sample_t *)jack_port_get_buffer(output_port, nframes);
+  for (i = 0; i < nframes; i++) {
+    fftw_in[i] = window(output[i], i);
+  }
+
   return 0;
 };
 
@@ -28,8 +44,13 @@ int main(int argc, char **argv) {
 
   input_port = jack_port_register(client, "input", JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
   output_port = jack_port_register(client, "output", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+  ports = jack_get_ports(client, NULL, NULL, JackPortIsPhysical | JackPortIsInput);
+  if(ports != NULL) {
+    jack_connect(client, jack_port_name(output_port), ports[0]);
+  }
 
   jack_set_process_callback(client, process, NULL);
+  jack_activate(client);
   for(;;){}
   return 0;
 }
